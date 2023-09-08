66ac7bbc25d4fa1c795836791332876fe9927c68
RSlack24 <97997249+RSlack24@users.noreply.github.com>
2023-09-08 18:09:49 -0400

    removed superfluous js

--- js/script.js
+++ js/script.js
@@ -25,44 +25,6 @@ $(document).ready(function () {
         window.location.href = "documents/Ryn_Slack_Resume.pdf";
     });
 
-    // Contact form code
-    $('#contactForm').submit(function (event) {
-        event.preventDefault();
-
-        const name = $('input[name="name"]').val();
-        const email = $('input[name="email"]').val();
-        const message = $('textarea[name="message"]').val();
-
-        // Basic email validation
-        if (!validateEmail(email)) {
-            alert("Please enter a valid email address.");
-            return;
-        }
-
-        // Send data to the PHP script hosted on a server
-        $.ajax({
-            type: 'POST',
-            url: 'https://your-server.com/your-php-script.php', // Replace with your server's URL
-            data: {
-                name: name,
-                email: email,
-                message: message
-            },
-            success: function (response) {
-                alert(response); // Display the server's response
-                $('#contactForm')[0].reset(); // Clear the form
-            },
-            error: function () {
-                alert("Oops! Something went wrong. Please try again later.");
-            }
-        });
-    });
-
-    function validateEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    }
-
 
 });
 
